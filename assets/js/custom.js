632b39eaafc9e8f48fc94482261475ad4072e3df
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-05-20 02:33:27 +0300

    Refactoring lang detect

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,7 @@
 // Language detect
 const url = window.location.href;
 const getLang = () => {
-    let lang = window.navigator.language;
-    if (/^en\b/.test(lang)) { lang = "en" } else { lang = "es" };
-    return lang;
+    return /^en\b/.test(window.navigator.language) ? "en" : "es";
 }
 document.onreadystatechange = () => {
     if (!(/es\b/.test(url)) && !(/en\b/.test(url))) {
