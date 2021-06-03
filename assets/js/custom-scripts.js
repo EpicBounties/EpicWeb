25c593ad4a68e0f5d395afe7a0ea0ad7b018d5c4
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-06-03 20:00:15 +0200

    Readding language select

--- assets/js/custom-scripts.js
+++ assets/js/custom-scripts.js
@@ -1,6 +1,16 @@
-
-  $(document).ready(function() {
-
+$(document).ready(function() {
+    // Language detect
+    var url = window.location.href;
+    if (!(/\bblog\b/.test(url))) {
+        function getLang() {
+            return /^en\b/.test(window.navigator.language) ? "en" : "es";
+        }
+        document.onreadystatechange = function() {
+            if (!(/\bes\b/.test(url)) && !(/\ben\b/.test(url))) {
+                window.location.href = url + getLang();
+            }
+        }
+    }
 
     /**
      * script for the the menu hamburger animation
@@ -15,82 +25,82 @@
      * @returns {}
      **/
     var forEach = function(t, o, r) {
-      if ("[object Object]" === Object.prototype.toString.call(t))
-        for (var c in t) Object.prototype.hasOwnProperty.call(t, c) && o.call(r, t[c], c, t);
-      else
-        for (var e = 0, l = t.length; l > e; e++) o.call(r, t[e], e, t)
+        if ("[object Object]" === Object.prototype.toString.call(t))
+            for (var c in t) Object.prototype.hasOwnProperty.call(t, c) && o.call(r, t[c], c, t);
+        else
+            for (var e = 0, l = t.length; l > e; e++) o.call(r, t[e], e, t)
     };
     // hamburger menu animation
     var hamburgers = document.querySelectorAll(".hamburger");
     if (hamburgers.length > 0) {
-      forEach(hamburgers, function(hamburger) {
-        hamburger.addEventListener("click", function() {
-          this.classList.toggle("is-active");
-        }, false);
-      });
+        forEach(hamburgers, function(hamburger) {
+            hamburger.addEventListener("click", function() {
+                this.classList.toggle("is-active");
+            }, false);
+        });
     }
 
     // run locomotive scroll
     window.addEventListener("load", function(event) {
-      const scroll = new LocomotiveScroll({
-        el: document.querySelector("[data-scroll-container]"),
-        smooth: true
-      });
+        const scroll = new LocomotiveScroll({
+            el: document.querySelector("[data-scroll-container]"),
+            smooth: true
+        });
     });
 
 
     // copy selected language value to the dropdown toggler
     function showLanguageOption(item) {
-      document.getElementById("dropdownLanguageOption").innerHTML = item.innerHTML;
+        document.getElementById("dropdownLanguageOption").innerHTML = item.innerHTML;
     }
 
     // Initiate OurTeam carousel
     function fnSwiperOurTeam() {
 
-      var swiperOurTeam = new Swiper("#swiperOurTeam", {
-        slidesPerView: 2,
-        spaceBetween: 0,
-
-        // Optional parameters
-        loop: true,
-
-        // Navigation arrows
-        navigation: {
-          nextEl: "#swiperOurTeam-next",
-          prevEl: "#swiperOurTeam-prev",
-        },
-
-        // responsive breakpoint
-        breakpoints: {
-          320: {
-            slidesPerView: 1,
-            spaceBetween: 0,
-          },
-          1200: {
+        var swiperOurTeam = new Swiper("#swiperOurTeam", {
             slidesPerView: 2,
             spaceBetween: 0,
-          },
-        }
-      }); /* END Swiper script */
+
+            // Optional parameters
+            loop: true,
+
+            // Navigation arrows
+            navigation: {
+                nextEl: "#swiperOurTeam-next",
+                prevEl: "#swiperOurTeam-prev",
+            },
+
+            // responsive breakpoint
+            breakpoints: {
+                320: {
+                    slidesPerView: 1,
+                    spaceBetween: 0,
+                },
+                1200: {
+                    slidesPerView: 2,
+                    spaceBetween: 0,
+                },
+            }
+        }); /* END Swiper script */
     } //fnSwiperOurTeam
 
     // Initiate Testimonials carousel
     function fnSwiperTestimonials() {
 
-      var swiperTestimonials = new Swiper("#swiperTestimonials", {
-        slidesPerView: 1,
-        spaceBetween: 0,
+        var swiperTestimonials = new Swiper("#swiperTestimonials", {
+            slidesPerView: 1,
+            spaceBetween: 0,
 
-        // Optional parameters
-        loop: true,
+            // Optional parameters
+            loop: true,
 
-        // Navigation arrows
-        navigation: {
-          nextEl: "#swiperTestimonials-next",
-          prevEl: "#swiperTestimonials-prev",
-        },
+            // Navigation arrows
+            navigation: {
+                nextEl: "#swiperTestimonials-next",
+                prevEl: "#swiperTestimonials-prev",
+            },
 
-      }); /* END Swiper script */
+        }); /* END Swiper script */
     } //fnSwiperOurTeam
 
 
@@ -98,9 +108,9 @@
     fnSwiperTestimonials();
 
     // run a preloader gif while waiting for the page to load
-    setTimeout(function(){
-      $("body").removeClass("is-loading");
-      $("#preloadingMask").fadeOut();
+    setTimeout(function() {
+        $("body").removeClass("is-loading");
+        $("#preloadingMask").fadeOut();
     }, 2000);
 
-  });
+});
