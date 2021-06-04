194ab75823d0b6dea7d325b1101bb245945a8088
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-06-04 14:11:43 +0200

    Adding old files to _backup/

---
+++ _backup/assets/js/custom.js
@@ -0,0 +1,132 @@
+// Language detect
+const url = window.location.href;
+const getLang = () => {
+    return /^en\b/.test(window.navigator.language) ? "en" : "es";
+}
+document.onreadystatechange = () => {
+    if (!(/\bes\b/.test(url)) && !(/\ben\b/.test(url))) {
+        window.location.href = url + getLang();
+    }
+}
+
+// Scroll
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 70) {
+        $('header').addClass("demo2");
+    } else {
+        $('header').removeClass("demo2");
+    }
+});
+
+// Cache selectors
+var lastId,
+    topMenu = $("#mainmenu"),
+    topMenuHeight = topMenu.outerHeight() + 15,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function() {
+        var item = $($(this).attr("href"));
+        if (item.length) { return item; }
+    });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e) {
+    var href = $(this).attr("href"),
+        offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
+    $('html, body').stop().animate({
+        scrollTop: offsetTop
+    }, 300);
+    e.preventDefault();
+});
+
+// Bind to scroll
+$(window).scroll(function() {
+    // Get container scroll position
+    var fromTop = $(this).scrollTop() + topMenuHeight;
+
+    // Get id of current scroll item
+    var cur = scrollItems.map(function() {
+        if ($(this).offset().top < fromTop)
+            return this;
+    });
+    // Get the id of the current element
+    cur = cur[cur.length - 1];
+    var id = cur && cur.length ? cur[0].id : "";
+
+    if (lastId !== id) {
+        lastId = id;
+        // Set/remove active class
+        menuItems
+            .parent().removeClass("active")
+            .end().filter("[href='#" + id + "']").parent().addClass("active");
+    }
+});
+
+
+
+
+
+
+
+// Cache selectors
+var lastId,
+    topMenu = $("#mainmenu"),
+    topMenuHeight = topMenu.outerHeight() + 15,
+    // All list items
+    menuItems = topMenu.find("a"),
+
+    // Get button
+    btn_get = $(".btn_get")
+
+// Anchors corresponding to menu items
+scrollItems = menuItems.map(function() {
+    var item = $($(this).attr("href"));
+    if (item.length) { return item; }
+});
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e) {
+    var href = $(this).attr("href"),
+        offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
+    $('html, body').stop().animate({
+        scrollTop: offsetTop
+    }, 300);
+    e.preventDefault();
+});
+
+// Bind click handler to get button
+// so we can get a fancy scroll animation
+btn_get.click(function(e) {
+    var href = $(this).attr("href"),
+        offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
+    $('html, body').stop().animate({
+        scrollTop: offsetTop
+    }, 300);
+    e.preventDefault();
+});
+
+// Bind to scroll
+$(window).scroll(function() {
+    // Get container scroll position
+    var fromTop = $(this).scrollTop() + topMenuHeight;
+
+    // Get id of current scroll item
+    var cur = scrollItems.map(function() {
+        if ($(this).offset().top < fromTop)
+            return this;
+    });
+    // Get the id of the current element
+    cur = cur[cur.length - 1];
+    var id = cur && cur.length ? cur[0].id : "";
+
+    if (lastId !== id) {
+        lastId = id;
+        // Set/remove active class
+        menuItems
+            .parent().removeClass("active")
+            .end().filter("[href='#" + id + "']").parent().addClass("active");
+    }
+});
